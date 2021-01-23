7d4f1d610348b9499190ab9da01db9dc6bd3afd3
Samuel <61194052+Strikza@users.noreply.github.com>
2021-01-23 18:40:26 +0100

    Revert "MAJ 1.03"
    This reverts commit 61cb5576029e6d000147a13dd88a6fc90589f2fd.

--- Code/PROJET.ml
+++ Code/PROJET.ml
@@ -13,9 +13,6 @@ open Random;;
 
 (**------------ Exercice 1 : Arbre Binaire de Recherche -------------**)
 
-
-(*=/1/============================================== *)
-
 let bst_rnd_create(sizeBorn : int): 'a bst =
   Random.self_init();
   let l : int list ref = ref [] and
@@ -28,28 +25,44 @@ let bst_rnd_create(sizeBorn : int): 'a bst =
 
 let test = bst_rnd_create(20);;
 show_int_btree(test);;
-let test = bst_rnd_create(20);;
-show_int_btree(test);;
 
 
-(*=/2/============================================== *)
+(* ================================================================== *)
+
+
+let max(x, y : int*int): int =
+  if(x<y)
+  then y
+  else x
+;;
+
+let rec height(btree : 'a t_btree): int =
+  if(isEmpty(btree))
+  then 0
+  else
+    let (ls, rs) = (lson(btree), rson(btree)) in
+    if(isEmpty(ls) && isEmpty(rs))
+    then 0
+    else
+      1 + max(height(ls), height(rs))
+;;
+
+height(test);;
 
 
 let compute_imbalance(): float =
   let sum : float ref = ref 0.0 and
       n = 10000 in
   for i=0 to n do
-    let t = bst_rnd_create(100) in
+    let t = bst_rnd_create(20) in
     sum := !sum +. float_of_int(height(lson(t)) - height(rson(t)));
    done;
   !sum/.float_of_int(n)
 ;;
 
 compute_imbalance();;
-(*  On remarque ici que lorsqu'on lance plusieurs fois la fonction, le
-(* résultat est presque systémtiquement le même, environ 0.
 
 
-(*=/3/============================================== *)
+(* ================================================================== *)
 
 
