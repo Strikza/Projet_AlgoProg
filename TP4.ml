fd36d344c9b17130541b2f9f71b2d7b9706b8f7e
Samuel <61194052+Strikza@users.noreply.github.com>
2021-01-23 14:23:42 +0100

    bst.ml

---
+++ TP4.ml
@@ -0,0 +1,29 @@
+#directory"G:/Cours_Univ/L3_Informatique/Semestre_5/Algo_Prog/Ressources/bytecodes/4.02.1+ocp1";; (* à lancer sur Windows *)
+#directory"/Volumes/Disque_Sam/Cours_Univ/L3_Informatique/Semestre_5/Algo_Prog/Ressources/bytecodes/4.10.0";;
+
+#load "btree.cmo";;
+open Btree;;
+
+(** EXERCICE 1 **)
+
+let test_bst : int bst = rooting(4,
+                                 rooting(3,
+                                         rooting(1, empty(), empty()),
+                                         rooting(2, empty(), empty())),
+                                 rooting(6,
+                                         rooting(5, empty(), empty()),
+                                         rooting(7, empty(), empty())))
+;;
+
+show_int_btree(test_bst);;
+#trace bst_seek;;
+root(bst_seek(test_bst, 8));;
+
+let test_bstBis = bst_linsert(test_bst, 8);;
+show_int_btree(test_bstBis);;
+root(bst_seek(test_bstBis, 8));;
+
+let l_bst = 2::3::1::5::6::[];; (* [6; 5; 1; 3; 2] *)
+let test_bstBisBis = bst_lbuild(l_bst);;
+show_int_btree(test_bstBisBis);;
+root(bst_seek(test_bstBisBis, 2));;
---
+++ bst.ml
@@ -0,0 +1,54 @@
+(*#directory"G:/Cours_Univ/L3_Informatique/Semestre_5/Algo_Prog/Ressources/bytecodes/4.02.1+ocp1";; (* à lancer sur Windows *)*)
+#directory"/Volumes/Disque_Sam/Cours_Univ/L3_Informatique/Semestre_5/Algo_Prog/Ressources/bytecodes/4.10.0";;
+
+#load "btree.cmo";;
+open Btree;;
+open List;;
+
+ 
+
+module type Bst =
+  sig
+    type 'a bst = 'a t_btree;;
+
+    val bst_seek : 'a bst * 'a -> 'a bst
+    val bst_linsert : 'a bst * 'a -> 'a bst
+    val bst_lbuild : 'a list -> 'a bst
+  end
+;;
+
+open Bst;;
+#show Bst;;
+
+let rec bst_seek(b, v : 'a bst * 'a): 'a bst =
+  if isEmpty(b)
+  then b
+  else
+    let (r, ls, rs) : ('a * 'a bst * 'a bst) = (root(b), lson(b), rson(b)) in
+
+    if r = v
+    then b
+    else
+      if r > v
+      then bst_seek(ls, v)
+      else bst_seek(rs, v)
+;;
+
+
+let rec bst_linsert(b, v : 'a bst * 'a): 'a bst =
+  if isEmpty(b)
+  then rooting(v, empty(), empty())
+  else
+    let (r, ls, rs) : ('a * 'a bst * 'a bst) = (root(b), lson(b), rson(b)) in
+
+    if r > v
+    then rooting(r, bst_linsert(ls, v), rs)
+    else rooting(r, ls, bst_linsert(rs, v))
+;;
+
+let rec bst_lbuild(l : 'a list): 'a bst =
+  if l = []
+  then empty()
+  else bst_linsert(bst_lbuild(tl(l)), hd(l))
+;;
+
