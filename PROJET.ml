03071b3c52846b9a672ec2c23bb4bee3b96a42b0
Hents <hents_a@outlook.fr>
2021-01-23 16:35:44 +0100

    Update PROJET.ml

--- PROJET.ml
+++ PROJET.ml
@@ -7,10 +7,15 @@ open Bst;;
 #show Bst;;
 
 open List;;
+
 open Random;;
 #show Random;;
 
-let randomBst(size : int): 'a bst =
+(**EXO1**)
+
+(*Q1*)
+
+let bst_rnd_create(size : int): 'a bst =
   Random.self_init();
   let l : int list ref = ref [] in
   for i=0 to size-1 do
@@ -21,3 +26,30 @@ let randomBst(size : int): 'a bst =
 
 let test = randomBst(8);;
 show_int_btree(test);;
+
+(*Q2*)
+
+let max(a, b) = Pervasives.max a b
+;;
+
+let rec height(tree : 'a t_btree) : int =
+  if ( isEmpty(tree) || (isEmpty(lson(tree)) && isEmpty(rson(tree))) )
+  then 0
+  else (
+   1 + max(height(lson(tree)), height(rson(tree)))
+  )
+;;
+
+
+let compute_imbalance(size: int): int =
+  let sum : int ref = ref 0 in
+  for i=0 to 20 do
+    let = bst_rnd_create(size) and
+        (lh,rh) = (height(lson(t)),height(rson(t))) in
+    sum := !sum + abs(lh-lr);
+   done;
+  sum := !sum/20
+;;
+
+   
+   
